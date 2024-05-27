b381f4729c321535687dabcb2160ba3a6b12bb77
Robin <mohammadrobin636@gmail.com>
2024-05-27 10:17:43 +0600

    Completed banner and about us section

--- src/sections/ServicesSlider.tsx
+++ src/sections/ServicesSlider.tsx
@@ -1,34 +1,69 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import { Navigation, Pagination, Virtual } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
+import { FaClipboardList } from "react-icons/fa6";
+import { MdDesktopWindows, MdPeopleAlt } from "react-icons/md";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 import "./ServicesSlider.css";
 
+interface TService {
+  title: string;
+  icon: ReactNode;
+}
+
 const ServicesSlider = () => {
   // Create array with 500 slides
-  const [slides] = useState(
-    Array.from({ length: 5 }).map((_, index) => `Slide ${index + 1}`)
-  );
+  const [slides] = useState<TService[]>([
+    {
+      title: "Third Party Monitoring",
+      icon: <MdDesktopWindows className="slideIcon" size={50} />,
+    },
+    {
+      title: "Midterm and endline Evaluations",
+      icon: <FaClipboardList className="slideIcon" size={50} />,
+    },
+    {
+      title: "Baseline surveys",
+      icon: <MdPeopleAlt className="slideIcon" size={50} />,
+    },
+    {
+      title: "Third Party Monitoring",
+      icon: <MdDesktopWindows className="slideIcon" size={50} />,
+    },
+    {
+      title: "Midterm and endline Evaluations",
+      icon: <FaClipboardList className="slideIcon" size={50} />,
+    },
+    {
+      title: "Baseline surveys",
+      icon: <MdPeopleAlt className="slideIcon" size={50} />,
+    },
+  ]);
 
   return (
-    <div className="h-[500px] border border-red-300">
+    <div className="h-[500px] overflow-visible">
       <Swiper
         modules={[Virtual, Navigation, Pagination]}
         slidesPerView={3}
-        initialSlide={1}
+        initialSlide={3}
         centeredSlides={true}
         navigation={true}
         virtual
       >
         {slides.map((slideContent, index) => (
-          <SwiperSlide key={slideContent} virtualIndex={index}>
-            <div className="border w-[80%] h-full">{slideContent}</div>
+          <SwiperSlide key={index} virtualIndex={index}>
+            <div className="border w-[440px] h-full slideItem">
+              <div className="w-full h-full flex flex-col justify-center items-center gap-5 p-[35px]">
+                <div className="slideIconBox">{slideContent.icon}</div>
+                <h2 className="slideHeading">{slideContent.title}</h2>
+              </div>
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
